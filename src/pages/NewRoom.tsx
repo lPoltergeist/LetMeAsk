d2f482a0ea7ca433ec501b749f08e009926b83f5
Gabriel José <punkj.ramone@live.com>
2022-04-25 09:40:04 -0300

    Add files via upload

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+import { FormEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import illustrationImg from '../assets/images/illustration.svg'; 
+import logoImg from '../assets/images/logo.svg'
+import { Link } from 'react-router-dom';
+import { database } from '../services/firebase';
+
+import { Button } from '../components/Button';
+
+import '../styles/auth.scss';
+import { useAuth } from '../hooks/useAuth';
+import DarkMode from '../components/DarkMode';
+
+
+export function NewRoom() {
+    const {user} = useAuth();
+    const [newRoom, setNewRoom] = useState('');
+    const navigate = useNavigate();
+   
+
+    async function handleCreateRoom(event: FormEvent) {
+event.preventDefault();
+
+if (newRoom.trim() === '') {
+    return;
+}
+
+const roomRef = database.ref('rooms');
+
+const firebaseRoom = await roomRef.push({
+    title: newRoom, 
+    authorId: user?.id,
+
+})
+
+navigate(`/rooms/${firebaseRoom.key}`)
+    }
+
+    return (
+        <div id="page-auth">
+            <aside>
+<img src={illustrationImg} alt="ilustração simbolizando perguntas e respostas" />
+<strong>Crie salas de Q&amp;A ao-vivo</strong>
+<p>Tire as dúvidas da sua audiência em tempo-real</p>
+            </aside>
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="Letmeask" />
+                    <h2>Criar uma nova sala</h2>
+                    <form onSubmit={handleCreateRoom}>
+                        <input type="text" 
+                        placeholder='Nome da sala'
+                        onChange={event => setNewRoom(event.target.value)}
+                        value={newRoom}/>
+                        <Button>Criar sala</Button>
+                    </form>
+                    <p> Quer entrar em uma sala existente? <Link to='/'>clique aqui</Link> 
+                    </p>
+                </div>
+            </main>
+      
+        </div>
+    )
+}
